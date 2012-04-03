2bf34bc87e73e0e063f5c41eba39b3d454dda0a9
map <manuel.paccagnella@gmail.com>
2012-04-03 22:30:53 +0200

    Basic items add

--- src/todojure/views/common.clj
+++ src/todojure/views/common.clj
@@ -1,6 +1,7 @@
 (ns todojure.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page-helpers :only [include-css html5]]))
+        [hiccup.page-helpers :only [include-css html5]]
+        [hiccup.form-helpers :only [form-to text-field submit-button]]))
 
 (defpartial layout [& content]
             (html5
@@ -12,10 +13,22 @@
                [:div#wrapper
                 content]]))
 
-(def items ["Hello" "Noir"])
+(def todo-list (atom ["Hello" "Noir"]
+                 :validator (fn [desc] (not (= "" desc)))))
 
 (defpartial todo-item [desc]
   [:li desc])
 
 (defpartial todo-items []
-  [:ol (map todo-item items)])
+  [:ol (map todo-item @todo-list)])
+
+(defpartial add-item-fields []
+  (text-field "desc"))
+
+(defpartial add-todo-form []
+  (form-to [:post "/"]
+           (add-item-fields)
+           (submit-button "Add")))
+
+(defn add-todo [desc]
+  (swap! todo-list conj desc))
--- src/todojure/views/index.clj
+++ src/todojure/views/index.clj
@@ -1,9 +1,14 @@
 (ns todojure.views.index
   (:require [todojure.views.common :as common])
-  (:use [noir.core :only [defpage]]
+  (:use [noir.core :only [defpage render]]
         [hiccup.core :only [html]]))
 
 (defpage "/" []
-         (common/layout
-          [:h1 "Todo list"]
-          (common/todo-items)))
+  (common/layout
+   [:h1 "Todo list"]
+   (common/todo-items)
+   [:hr]
+   (common/add-todo-form)))
+
+(defpage [:post "/"] {:as item}
+  (do (common/add-todo (:desc item)) (render "/")))
