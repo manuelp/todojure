e893bda9b9d2559a53feab17a9b34950cc3fb73c
map <manuel.paccagnella@gmail.com>
2012-04-05 21:58:37 +0200

    Implemented a bunch of function for both functional and mutable models

--- src/todojure/core.clj
+++ src/todojure/core.clj
@@ -1,8 +1,25 @@
 (ns todojure.core)
 
+;; ## Functional model ##
+(defn do-mark [items desc state]
+  (map #(if (= (:desc %) desc)
+          (assoc % :marked state)
+          %)
+       items))
+
+(defn small-list [list]
+  (filter :marked (reverse list)))
+
+(defn remove-item [items desc]
+  (remove #(= (:desc %) desc) items))
+
+(defn reset-all [items]
+  (map #(assoc % :marked false) items))
+
 ;; ## Mutable model ##
-(def master-list (atom [{:desc "Hello" :marked false} {:desc "Noir" :marked true}]
-                       :validator (fn [{:keys [desc]}] (not (= "" desc)))))
+(def master-list (atom [{:desc "Hello" :marked false}
+                        {:desc "Noir" :marked true}]
+                       :validator (partial every? #(not= "" (:desc %)))))
 
 (defn add-todo [txt]
   (swap! master-list conj {:desc txt :marked false}))
@@ -13,12 +30,11 @@
 (defn unmark [desc]
   (swap! master-list do-mark desc false))
 
-;; ## Functional model ##
-(defn do-mark [items desc state]
-  (map #(if (= (:desc %) desc)
-          (assoc % :marked state)
-          %)
-       items))
+(defn done [desc]
+  (swap! master-list remove-item desc))
 
-(defn small-list []
-  (filter :marked (reverse @master-list)))
+(defn compile-small-list []
+  (small-list @master-list))
+
+(defn reset []
+  (reset-all @master-list))
